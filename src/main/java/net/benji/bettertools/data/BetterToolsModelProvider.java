0da16acf6519ab7bc180da4aea0c233422d4c761
Blander24 <benlands101@gmail.com>
2025-12-13 18:39:21 -0500

    Updated to 1.21.4

--- src/main/java/net/benji/bettertools/data/BetterToolsModelProvider.java
+++ src/main/java/net/benji/bettertools/data/BetterToolsModelProvider.java
@@ -2,12 +2,12 @@ package net.benji.bettertools.data;
 
 import net.benji.bettertools.block.BetterToolsBlocks;
 import net.benji.bettertools.item.BetterToolsItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.models.BlockModelGenerators;
-import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.data.models.model.ModelTemplates;
-import net.minecraft.data.models.model.TexturedModel;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TexturedModel;
 
 public class BetterToolsModelProvider extends FabricModelProvider {
     public BetterToolsModelProvider(FabricDataOutput output) {
