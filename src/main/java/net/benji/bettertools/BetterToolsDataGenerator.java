345a04db0fb385b138f0b8feb80203950b49da29
Blander24 <benlands101@gmail.com>
2025-12-13 12:00:02 -0500

    Initial commit

---
+++ src/main/java/net/benji/bettertools/BetterToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.benji.bettertools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
