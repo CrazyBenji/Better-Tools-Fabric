a4cf74a322b4a5d500919bd00265f591f7ade10f
Blander24 <benlands101@gmail.com>
2026-08-01 00:41:03 -0400

    Fixed bug in WoodChiselingRecipe

--- src/main/java/net/benji/bettertools/item/crafting/WoodChiselingRecipe.java
+++ src/main/java/net/benji/bettertools/item/crafting/WoodChiselingRecipe.java
@@ -63,6 +63,9 @@ public class WoodChiselingRecipe extends CustomRecipe {
                     if (logBlock.isPresent()) {
                         log = true;
                     }
+                    else {
+                        return false;
+                    }
                 }
                 else {
                     return false;
